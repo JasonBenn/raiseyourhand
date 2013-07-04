05c237502b766803fddc5ad5f743a8ece1d5a72f
Jason Benn <jasoncbenn@gmail.com>
2013-07-04 16:22:06 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raiseyourhand::Application.config.secret_token = '6aa4479be92e5e5088062513114d5e3ca9c2c5406931611fc391c440473d4a525b744df7876984d05ba780d024e3c726fc522e78b85d1fe1d67ddc7e86550f76'
