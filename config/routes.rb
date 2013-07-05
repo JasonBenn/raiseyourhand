0660457ae43a87cae7d29cee9c68b478e02586bc
Aaron Dufall <aald212@gmail.com>
2013-07-04 20:29:59 -0500

    sign in,out and root routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Raiseyourhand::Application.routes.draw do
+  root to: 'lessons#index'
+  match 'auth/:provider/callback', to: 'sessions#create'
+  match 'auth/failure', to: redirect('/')
+  match 'signout', to: 'sessions#destroy', as: 'signout' 
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
