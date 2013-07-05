434bf6eabc2903585f1cb7772234bcf3569e9a13
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-05 14:53:51 -0500

    change attr accessible

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,5 +1,5 @@
 class Content < ActiveRecord::Base
-  attr_accessible :lesson_id, :url
+  attr_accessible :lesson_id, :url, :position, :start_time, :finish_time
   belongs_to :lesson
   has_many :questions
   has_many :flashcards
