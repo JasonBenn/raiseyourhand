b8befd31ca8019247681a4326205b767d1a2bc83
Jason Benn <jasoncbenn@gmail.com>
2013-07-04 19:57:30 -0500

    user validation specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,4 +14,11 @@ describe User do
     it { should allow_mass_assignment_of(:first_name) }
     it { should allow_mass_assignment_of(:last_name) }
   end
+
+  context 'testing validations' do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should_not allow_value('bad@email').for(:email) }
+    it { should allow_value('good@email.com').for(:email) }
+  end
 end
