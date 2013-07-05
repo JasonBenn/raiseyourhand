ee13149ce33bf5af557fe877c5da29b55b07c622
Aaron Dufall <aald212@gmail.com>
2013-07-04 19:50:07 -0500

    resolved merge conflicts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,21 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :first_name, :last_name
+  attr_accessible :email, :oauth_token, :preference, :profile_id
   has_many :user_lessons
   has_many :lessons, through: :user_lessons
   has_many :created_lessons, class_name: 'Lesson', foreign_key: 'creator_id'
+
+  validates :oauth_token, presence: true
+
+  def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
+      user.set_profile
+    end
+  end
+>>>>>>> Stashed changes
 end
