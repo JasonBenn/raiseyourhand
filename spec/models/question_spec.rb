0d42c0e73e71f2e8f6a5fe2ba347b6f121287693
Jason Benn <jasoncbenn@gmail.com>
2013-07-04 20:24:38 -0500

    question unit tests

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Question do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'testing associations' do
+    it { should belong_to(:content) }
+    it { should have_many(:answers) }
+  end
+
+  context 'testing attr_accessible' do
+    it { should allow_mass_assignment_of(:content_id) }
+    it { should allow_mass_assignment_of(:text) }
+    it { should allow_mass_assignment_of(:time_in_content) }
+  end
+
+  pending 'testing validations'
 end
