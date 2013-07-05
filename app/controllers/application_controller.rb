918888be4abbe53b8976df5f8143bd2e87e2a53d
Aaron Dufall <aald212@gmail.com>
2013-07-04 20:24:52 -0500

    added user authentication  helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+
+  private
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
+  helper_method :current_user
+
+  def is_logged_in?
+  	!current_user.nil?
+  end
+  helper_method :is_logged_in?
+
+  def authenticated
+  	redirect_to root_url unless is_logged_in?
+  end
 end
