2d5a0a0fd21dc9a461ebd5038b34769cc098b62c
Jason Benn <jasoncbenn@gmail.com>
2013-07-04 19:45:01 -0500

    user associations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'testing associations' do
+    it { should have_many :user_lessons }
+    it { should have_many :user_lessons }
+    it { should have_many(:lessons).through(:user_lessons) }
+    it { should have_many(:created_lessons).class_name('Lesson') }
+  end
+
+  
+  # attr_accessible :email, :first_name, :last_name
 end
