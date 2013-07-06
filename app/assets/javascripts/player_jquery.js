4cf09e1cb820e9f92539addbb3bc429aeb9aa721
Lukas Ingelheim <Lukas@Lukass-MacBook-Pro.local>
2013-07-06 17:25:24 -0500

    got basic scroll for dummy questions working. Incl cleaned up layout and refactored code

--- app/assets/javascripts/player_jquery.js
+++ app/assets/javascripts/player_jquery.js
@@ -1,64 +1,51 @@
 $(document).ready(function(){
-// alert("here");
-$("#ask_question").mouseenter(function(){
-  // alert("here");
- pauseVideo();
-  addTiming();
+
+  $("#ask_question").mouseenter(function(){
+   pauseVideo();
+ });
+
+  $("#questions-answers").mouseenter(function(){
+   pauseVideo();
+ });
+
+  $(".qcontainer").mouseenter(function(){
+    $(this).children(".triangle-border").css("heigth", "auto");
+    $(this).children(".triangle-border").children(".question_body").slideDown();
+    $(this).siblings(".repective-answers").children(".acontainer").slideDown();
+    $(this).siblings(".repective-answers").css("margin-top", "150px");
   });
 
-$(".question").click(function(){
-  // alert("here");
-$(this).children("ul").children("li").slideDown();
-// alert($(this).parent("ul").position().top);
-// $(".questions-answers").scrollTop(300);
-// alert($(this).parent("ul").position().top*-1);
-var position = Math.round($(this).parent("ul").position().top*-1);
-// alert(position);
+  $(".qcontainer").mouseleave(function(){
+    $(this).children(".triangle-border").css("heigth", "50px");
+    $(this).children(".triangle-border").children(".question_body").hide();
+    $(this).siblings(".repective-answers").children(".acontainer").hide();
+    $(this).siblings(".repective-answers").css("margin-top", "0px");
+  });
 
-$(".questions-answers").scrollTop(position);
+  $("#ask_question").mouseleave(function(){
+    playVideo();
+  });
+
+  $("#questions-answers").mouseleave(function(){
+    playVideo();
+  });
+
+  var $progressBarContainer = $('.progress-bar');
+  var $progressBarStatus = $('.progress')
+
+  function getProgressTimeRequest(e){
+    var parentOffsetX = $progressBarContainer.offset().left;
+    var mouseX = e.pageX;
+    var relativeX =  mouseX - parentOffsetX;
+    var mousePercentage = relativeX / $progressBarContainer.width();
+    return ytplayer.getDuration() * mousePercentage;
+  }
+
+  $('.progress-bar').click(function(e){
+    newTime = getProgressTimeRequest(e)
+    seekTo(newTime);
+  });
 });
-
-$(".question").on('dblclick', function(){
-  // alert("here");
-$(this).children("ul").children("li").slideUp();
-
-
-});
-
-
-
-
-
-
-$("#ask_question").mouseleave(function(){
-  playVideo();
-});
-
- var $progressBarContainer = $('.progress-bar');
-      var $progressBarStatus = $('.progress')
-      // var $progressSlide = $('.draggable-progress');
-
-      function getProgressTimeRequest(e){
-        var parentOffsetX = $progressBarContainer.offset().left;
-        var mouseX = e.pageX;
-        var relativeX =  mouseX - parentOffsetX;
-        var mousePercentage = relativeX / $progressBarContainer.width();
-        return ytplayer.getDuration() * mousePercentage;
-      }
-
-      $('.progress-bar').click(function(e){
-        newTime = getProgressTimeRequest(e)
-        seekTo(newTime);
-      });
-
-
-});
-
-
-var addTiming = function(){
-updateValue("time", ytplayer.getCurrentTime());
-};
 
 var stopTimer = function(){
 };
-
