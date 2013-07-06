ef5b0c84ea3c28b7337116fba27e00478e257977
Aaron Dufall <aald212@gmail.com>
2013-07-05 21:55:02 -0500

    validates the presence of associated object

--- app/models/content.rb
+++ app/models/content.rb
@@ -3,7 +3,8 @@ class Content < ActiveRecord::Base
   belongs_to :lesson
   has_many :questions
   has_many :flashcards
-
+  validates_presence_of :lesson
+  validates_associated :lesson
 
   def length
     finish_time.to_f - start_time.to_f
