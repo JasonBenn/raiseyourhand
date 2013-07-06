b7a941e3ba5b1168454b70aa694a1ad399069413
Aaron Dufall <aald212@gmail.com>
2013-07-05 19:24:27 -0500

    fixed spelling mistakes

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,6 +1,7 @@
 class Content < ActiveRecord::Base
-  attr_accessible :lesson_id, :url
+  attr_accessible :lesson_id, :url, :start_time, :finish_time, :position
   belongs_to :lesson
   has_many :questions
   has_many :flashcards
+
 end
