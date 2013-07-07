494ad72b9515293e9b0d3ef352482f1870f6693c
Aaron Dufall <aald212@gmail.com>
2013-07-07 16:09:30 -0500

    Added utiliteis module with nested hash key search

---
+++ lib/utilities.rb
@@ -0,0 +1,11 @@
+module Utilties
+	def self.nested_hash_value(obj,key)
+	  if obj.respond_to?(:key?) && obj.key?(key)
+	    obj[key]
+	  elsif obj.respond_to?(:each)
+	    r = nil
+	    obj.find { |*a| r = nested_hash_value(a.last,key) }
+	   	r
+	  end
+	end
+end
