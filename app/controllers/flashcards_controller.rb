76d3c80bbf297427dc06c982d8a6f5980091b8a7
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-07 19:16:45 -0500

    flashcard new form and controller action

--- app/controllers/flashcards_controller.rb
+++ app/controllers/flashcards_controller.rb
@@ -3,4 +3,9 @@ class FlashcardsController < ApplicationController
     @flashcards = Content.find(params[:content_id]).flashcards
     render partial: 'flashcards/data'
   end
+
+  def create
+    flashcard = Flashcard.create(params[:flashcard])
+    render text: flashcard.content.flashcards.count
+  end
 end
