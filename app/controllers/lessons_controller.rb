f67bd37f21f49211f2fef80fc141a8b75693cfff
Aaron Dufall <aald212@gmail.com>
2013-07-09 14:02:59 -0500

    removed unused methods

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -46,31 +46,5 @@ class LessonsController < ApplicationController
 	def update
 	end
 
-	private
-
-	def getMetaDataFromYoutubeWithId(youtube_id)
-		JSON.parse(open("http://gdata.youtube.com/feeds/api/videos/#{youtube_id}?v=2&alt=json&prettyprint=true").read)
-	end
-
-	def getMetaDataFromYoutubeWithUrl(url)
-		id = getVideoIdFromUrl(url)
-		getMetaDataFromYoutubeWithId(id)
-	end
-
-	def get_youtube_title(url)
-		id = getVideoIdFromUrl(url)
-		data = getMetaDataFromYoutube(id)
-		data['entry']['title']['$t']
-	end
-
-	def get_youtube_duration(youtube_id)
-		data = getMetaDataFromYoutubeWithUrl(youtube_id)
-		data['entry']['media$group']['yt$duration']['seconds']
-	end
-
-	def getVideoIdFromUrl(url)
-		url_params = CGI.parse(URI.parse(url).query) 
-		url_params['v'][0]
-	end
 end
 
