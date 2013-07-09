e7e85a8f066e89f9bbe21548a00081863fa58afc
Aaron Dufall <aald212@gmail.com>
2013-07-09 10:13:54 -0500

    configure oauth for testing

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -6,7 +6,6 @@ Raiseyourhand::Application.configure do
   # your test database is "scratch space" for the test suite and is wiped
   # and recreated between test runs. Don't rely on the data there!
   config.cache_classes = true
-
   # Configure static asset server for tests with Cache-Control for performance
   config.serve_static_assets = true
   config.static_cache_control = "public, max-age=3600"
@@ -34,4 +33,6 @@ Raiseyourhand::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+  # Stub out oauth in testing
+  OmniAuth.config.test_mode = true
 end
