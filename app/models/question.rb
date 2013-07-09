7c1af7cf3bcb357dd055c7f467da827b61ff70da
Jason Benn <jasoncbenn@gmail.com>
2013-07-09 11:01:39 -0500

    reformatting models

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,10 +3,12 @@ class Question < ActiveRecord::Base
   include Indexer
 
   attr_accessible :content_id, :text, :time_in_content, :title
+
   belongs_to :content
+  delegate :lesson_id, to: :content
+  belongs_to :searchable, polymorphic: true
   has_many :answers
   has_many :votes, as: :votable
-  belongs_to :searchable, polymorphic: true
 
   after_save :index_attributes
 end
