bc1d6c77c4e05aa0bb0ae324278f6c37f0d2f835
Jason Benn <jasoncbenn@gmail.com>
2013-07-08 19:52:55 -0500

    search searches table with user input

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,4 +1,7 @@
 class Search < ActiveRecord::Base
   attr_accessible :searchable_id, :searchable_type, :term
   belongs_to :searchable, polymorphic: true
+  def self.search(query)
+  	where('term LIKE ?', '%' + query + '%')
+  end
 end
