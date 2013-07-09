c77e0baab0ccab890ff5d463f7381ee5ef0f8299
Jason Benn <jasoncbenn@gmail.com>
2013-07-08 23:02:25 -0500

    eager loading on searchables

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,6 @@
 class SearchesController < ApplicationController
 	def search
-		results = Search.search(params[:search])
+		results = Search.search(params[:search]).includes(:searchable)
 		records = results.map(&:searchable)
 		render partial: 'results', locals: { records: records }
 	end
