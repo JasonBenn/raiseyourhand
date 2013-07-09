5d634b7054af8a463bbefd8ae1f50024d8f854ce
Jason Benn <jasoncbenn@gmail.com>
2013-07-08 23:02:37 -0500

    validation causes bugs?

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,7 +1,7 @@
 class Search < ActiveRecord::Base
   attr_accessible :searchable_id, :searchable_type, :term
   belongs_to :searchable, polymorphic: true
-  validates_uniqueness_of :term, scope: [:searchable_id, :searchable_type]
+  # validates_uniqueness_of :term, scope: [:searchable_id, :searchable_type]
 
   def self.search(query)
   	where('term LIKE ?', '%' + query + '%')
