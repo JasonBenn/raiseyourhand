ab0ed4808f3eda3dd656e58cf869ac0a79bb4660
Jason Benn <jasoncbenn@gmail.com>
2013-07-09 10:59:03 -0500

    replace $.get with $.ajax

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
-	$('#search').keyup(function() {
-    $('.title').hide();
-    $('#search').removeClass('search-big-mode')
-    $('#search').addClass('search-small-mode')
-    search = $('#search').val();
-    $.get('/search', { 'search': search }, function(response) {
-      $('#results').empty()
-      $('#results').html(response)
-      $('#tabs').tabs();
-    })
+    if ($('#search').val().length > 2) {
+      $('.title').hide();
+      $('#search').removeClass('search-big-mode')
+      $('#search').addClass('search-small-mode')
+      search = $('#search').val();
+
+      $.ajax('/search', {
+        data: { 'search': search }, 
+        success: function(response) {
+          $('#results').empty()
+          $('#results').html(response)
+          $('#tabs').tabs();
+        }
+      });
   })
 })
