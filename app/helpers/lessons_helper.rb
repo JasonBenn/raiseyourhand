99a7223c8b8689fdfa2c8becf3d80113970dbac8
Jason Benn <jasoncbenn@gmail.com>
2013-07-09 15:24:42 -0500

    extract find user lesson to helper

--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -4,4 +4,8 @@ module LessonsHelper
       video.questions.each {|question| times[question.time_in_content.to_f.ceil] = question} 
       times
 	end
+
+	def find_user_lesson(user_id, lesson_id)
+		UserLesson.find_by_user_id_and_lesson_id(user_id, lesson_id)
+	end
 end
