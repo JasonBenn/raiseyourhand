1d7d30ad495b3d2558e70c134b5d5111892278c7
Aaron Dufall <aald212@gmail.com>
2013-07-09 09:27:55 -0500

    pending lesson specs

--- spec/controllers/lessons_spec.rb
+++ spec/controllers/lessons_spec.rb
@@ -3,13 +3,8 @@ require 'spec_helper'
 
 describe LessonsController do
 	describe '#create' do
-		it 'a lesson when signed in' do
-			post(:create, title: "Oh Yeah", 
-										contents_attributes:
-										{0 =>{position: "0", 
-													start_time: "0", 
-													url: "http://www.youtube.com/watch?v=NkVsJGl5d6E"})
-		end
+		it 'a lesson when signed in'
+
 
 		it 'should create the first piece of content'
 
