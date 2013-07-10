d549a674d427ecc20081b52e2f54e5adc3019d2d
Aaron Dufall <aald212@gmail.com>
2013-07-10 09:23:31 -0500

    fixed double create bug

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,7 +2,7 @@ class AnswersController < ApplicationController
 	def create
 		@answer = current_user.answers.build(params[:answer])
   	if @answer.save
-    	render json: Answer.create(params[:answer]), status: '201'
+    	render json: @anwers, status: '201'
     else
     	bad_request
     end
