3b4f43e674fe79d2f8327655c2d9bac69d58a00f
Aaron Dufall <aald212@gmail.com>
2013-07-09 20:14:37 -0500

    users can create questions and answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,5 +2,6 @@ class Answer < ActiveRecord::Base
   include VotableHelper
   attr_accessible :question_id, :text
   belongs_to :question, counter_cache: true
+  belongs_to :user
   has_many :votes, as: :votable
 end
