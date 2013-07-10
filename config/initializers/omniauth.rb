001ffad8199656981aef5b58415a10d17c85698a
Aaron Dufall <aald212@gmail.com>
2013-07-10 09:00:05 -0500

    added test env

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -17,3 +17,17 @@ if Rails.env.production?
            			:scope => 'email', :display => 'popup'
  	end
 end
+
+
+if Rails.env.test?
+	FACEBOOK_CONFIG = YAML.load_file("#{::Rails.root}/config/facebook.yml")[::Rails.env]
+
+	OmniAuth.config.logger = Rails.logger
+
+	Rails.application.config.middleware.use OmniAuth::Builder do
+  	provider 	:facebook, FACEBOOK_CONFIG['app_id'], FACEBOOK_CONFIG['secret'],
+           		:scope => 'email', :display => 'popup'
+ 		end
+end
+
+
