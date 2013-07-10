e0ace8d52b3318fd7040578471e7f63711339228
Jordan Nemrow <nemrowj@gmail.com>
2013-07-10 12:57:13 -0500

    my_lessons view is looking nice and functional

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 	$('.toggle-button').click(function() {
-		$(this).prev().slideToggle();
+		if ($(this).attr('data-opened') == 'true'){
+			$(this).css({'background-position':'0px 0px', 'bottom':'-9px'}).attr('data-opened', 'false');
+		} else {
+			$(this).css({'background-position':'0px -30px', 'bottom':'-6px'}).attr('data-opened', 'true');
+		};
+		$(this).closest('.lesson-group').children('.lesson-group-bottom').slideToggle();
 	});
 });
